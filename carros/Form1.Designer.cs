e49a9911d5d35d8c1d3ef0f737c90b56381e6c9b
eduardasnz <meduardasan18@hotmail.com>
2024-11-22 14:02:09 -0300

    Primeiro commit do projeto Carros

---
+++ carros/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace carros
+{
+    partial class Carros
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MarcaCarro = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.ModeloCarro = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MarcaCarro
+            // 
+            this.MarcaCarro.AutoSize = true;
+            this.MarcaCarro.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MarcaCarro.Location = new System.Drawing.Point(159, 35);
+            this.MarcaCarro.Name = "MarcaCarro";
+            this.MarcaCarro.Size = new System.Drawing.Size(77, 27);
+            this.MarcaCarro.TabIndex = 0;
+            this.MarcaCarro.Text = "Marca";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Fiat",
+            "Chevrolet",
+            "Bmw",
+            "Honda",
+            "Toyota"});
+            this.comboBox1.Location = new System.Drawing.Point(143, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(357, 65);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectIndexChanged);
+            // 
+            // ModeloCarro
+            // 
+            this.ModeloCarro.AutoSize = true;
+            this.ModeloCarro.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModeloCarro.Location = new System.Drawing.Point(370, 35);
+            this.ModeloCarro.Name = "ModeloCarro";
+            this.ModeloCarro.Size = new System.Drawing.Size(90, 27);
+            this.ModeloCarro.TabIndex = 4;
+            this.ModeloCarro.Text = "Modelo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(105, 164);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Carros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 385);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ModeloCarro);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.MarcaCarro);
+            this.Name = "Carros";
+            this.Text = "Carros";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MarcaCarro;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label ModeloCarro;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ carros/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace carros
+{
+    public partial class Carros : Form
+    {
+        public Carros()
+        {
+            InitializeComponent();
+        }
+
+        public void comboBox2_SelectIndexChanged(object sender, EventArgs e)
+        {
+            string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img");
+            string imageFile = Path.Combine(imagePath, comboBox2.Text + ".png");
+
+            //MessageBox.Show("Tentando carregar " + imageFile);
+
+            if (File.Exists(imageFile))
+            {
+                pictureBox1.ImageLocation = imageFile;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Imagem não encontrada!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        public void comboBox1_SelectIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            if(comboBox1.Text == "Fiat")
+            {
+                comboBox2.Items.Add("Argo");
+                comboBox2.Items.Add("Toro");
+            } 
+            
+            else if (comboBox1.Text == "Chevrolet")
+            {
+                comboBox2.Items.Add("Onix");
+                comboBox2.Items.Add("Camaro");
+            }
+
+            else if (comboBox1.Text == "Honda")
+            {
+                comboBox2.Items.Add("Civic");
+                comboBox2.Items.Add("HRV");
+            }
+
+            else if (comboBox1.Text == "Toyota")
+            {
+                comboBox2.Items.Add("Corolla");
+                comboBox2.Items.Add("Yaris");
+            }
+
+            else if (comboBox1.Text == "Bmw")
+            {
+                comboBox2.Items.Add("M3");
+                comboBox2.Items.Add("320i");
+            }
+
+        }
+        /*
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MarcaCarro_Click(object sender, EventArgs e)
+        {
+
+        }
+        */
+    }
+}
